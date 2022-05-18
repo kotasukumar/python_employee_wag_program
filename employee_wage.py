9725b66ba575f9da164bf1b65f2aff5ccdf2bd46
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-05-18 17:44:58 +0530

    attendence

--- employee_wage.py
+++ employee_wage.py
@@ -1,1 +1,14 @@
-print("welcome to employee wage program")
+import random
+
+
+def check_attendence(random_number):
+    if random_number == 0:
+        return "Absent"
+    else:
+        return "Present"
+
+
+if __name__ == "__main__":
+    random = random.randint(0, 1)
+    attendence = check_attendence(random)
+    print(attendence)
