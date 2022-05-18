b5b1c8129fc2104a988f487dd2cd97cbc259b486
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-05-18 18:55:23 +0530

    while case

--- employee_wage.py
+++ employee_wage.py
@@ -1,18 +1,24 @@
 import random
 
 
-def check_attendence(random_number):
-    if random_number == 0:
-        daily_wage = 8 * 20
-    elif random_number == 1:
-        daily_wage = 8 * 20
-    else:
-        daily_wage = 0
+def check_attendence(total_days):
+    daily_wage = present_days = working_hours = 0
+    while present_days != 20 and working_hours != 100:
+        random_number = random.randint(0, 2)
+        if random_number == 0:
+            daily_wage += (8 * 20)
+            present_days += 1
+            working_hours += 8
+        elif random_number == 1:
+            daily_wage += (8 * 20)
+            present_days += 1
+            working_hours += 8
+        else:
+            daily_wage += 0
 
     return daily_wage
 
 
 if __name__ == "__main__":
-    random = random.randint(0, 2)
-    wage = check_attendence(random)
+    wage = check_attendence(20)
     print(wage)
