e72e5f1e3c72d492d95569b23e3bce74c484f742
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-05-18 19:18:59 +0530

    last uc

--- employee_wage.py
+++ employee_wage.py
@@ -1,24 +1,26 @@
 import random
 
 
-def check_attendence(total_days):
-    daily_wage = present_days = working_hours = 0
+def check_attendence():
+    daily_wage_list = []
+    daily_wage = present_days = working_hours = i = 0
     while present_days != 20 and working_hours != 100:
         random_number = random.randint(0, 2)
-        if random_number == 0:
+        if random_number == 0 or random_number == 1:
             daily_wage += (8 * 20)
             present_days += 1
             working_hours += 8
-        elif random_number == 1:
-            daily_wage += (8 * 20)
-            present_days += 1
-            working_hours += 8
+            i += 1
+            daily_wage_list.append(8 * 20)
         else:
             daily_wage += 0
+            daily_wage_list.append(0)
 
-    return daily_wage
+    return daily_wage, daily_wage_list
 
 
 if __name__ == "__main__":
-    wage = check_attendence(20)
-    print(wage)
+    wage, wage_list = check_attendence()
+    for i in range(len(wage_list)):
+        print("day ", i, wage_list[i])
+    print("monthly wage: ", wage)
